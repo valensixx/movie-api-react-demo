d4f12c93ba5b5a806c133f09a4f7bf1271c570cd
valensixx <valensixx@gmail.com>
2023-05-02 16:07:40 +0300

    api key working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './App.css';
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
@@ -6,14 +6,26 @@ import SearchBar from "./components/SearchBar";
 /*
 Task 9 --> Movie search
 Create app for movie search. Use https://www.omdbapi.com/.
-key=4490a55c
 */
 
-export default function App(){
-  return(
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function searchMovie() {
+    fetch(`https://www.omdbapi.com/?apikey=4490a55c&t=${searchTerm}`)
+      .then(response => response.json())
+      .then(movie => {
+        console.log(movie);
+      })
+  }
+
+  return (
     <div className="App">
-        <Header />
-        <SearchBar />
+      <Header />
+      <SearchBar
+        setSearchTerm={setSearchTerm}
+        onButtonClick={searchMovie}
+      />
     </div>
   );
 }
