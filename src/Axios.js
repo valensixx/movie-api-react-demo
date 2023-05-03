c039c4a1948649b2c2e79494eed92ed664490b72
valensixx <valensixx@gmail.com>
2023-05-03 14:36:16 +0300

    Axios.js jsonplaceholder request task

---
+++ src/Axios.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+/*
+    Do api request to: https://jsonplaceholder.typicode.com/users
+*/
+
+export default function Axios() {
+    return (
+        <div className="Axios">
+
+        </div>
+    );
+}
